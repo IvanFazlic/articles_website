01f62da42b2dfb5fba70f4b4f7dcbdc2defdd7c7
Ivan <fazlic.ivan00@gmail.com>
2024-10-16 12:05:24 -0400

    Fix bug

--- src/db/index.tsx
+++ src/db/index.tsx
@@ -2,7 +2,13 @@ import { PrismaClient, Articles, Categories } from "@prisma/client";
 export type ArticleKeys = keyof Articles;
 export type CategoryKeys = keyof Categories;
 
-export const db = new PrismaClient();
+export const db = new PrismaClient({
+    datasources:{
+        db:{
+            url : process.env.DATABASE_URL,
+        }
+    }
+});
 
 export const articleKeys: ArticleKeys[] = Object.keys(db.articles.fields) as (keyof Articles)[];
 export const categoryKeys: CategoryKeys[] = Object.keys(db.categories.fields) as CategoryKeys[];

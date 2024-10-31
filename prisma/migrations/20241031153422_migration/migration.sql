608c7b1e3cd790035b101950bb90647bc6229ba1
Ivan <fazlic.ivan00@gmail.com>
2024-10-31 11:44:57 -0400

    Test in production 31.10.2024.

--- prisma/migrations/20241031153422_migration/migration.sql
+++ prisma/migrations/20241031153422_migration/migration.sql
@@ -1,22 +1,28 @@
 -- CreateTable
 CREATE TABLE "Categories" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "category" TEXT NOT NULL,
-    "image" TEXT NOT NULL
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "Categories_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Articles" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "topImage" TEXT NOT NULL,
     "middleImage" TEXT NOT NULL,
     "bottomImage" TEXT NOT NULL,
     "paragraphText" TEXT NOT NULL,
     "categoryId" INTEGER NOT NULL,
-    CONSTRAINT "Articles_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Categories" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "Articles_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Categories_category_key" ON "Categories"("category");
+
+-- AddForeignKey
+ALTER TABLE "Articles" ADD CONSTRAINT "Articles_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
